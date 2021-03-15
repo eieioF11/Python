e47c427fa9e780ea5fc0016f9be3a2ee991a5efd
Nishizawa Eiji <eieioeiji0501@gmail.com>
2021-03-15 15:02:44 +0900

    Added log creation function

--- project/Load_Cell/main.py
+++ project/Load_Cell/main.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
+from SerialConnection import select_port
 import numpy as np
-import serial
+import datetime
 
 def main():
-    print("Start")
-    ser = serial.Serial('com3',9600,timeout=None)
+    print("Start!")
+    ser = select_port()
+    if ser is None:
+        return
+
+    dt_now = datetime.datetime.now()
+    path='data/'+dt_now.strftime('%Y%m%d%H%M%S_Log.csv')
+    f = open(path, 'w')
 
     plt.figure()
     #x軸の幅を100にする
@@ -15,35 +22,49 @@ def main():
     while True:
         try:
             ser.reset_input_buffer()
-            data=ser.readline()
+            ser.write(b'F')
+            readdata=str(ser.readline().decode())
+            data=readdata.replace('\r\n','')
+            sensordata=data.split(',')
             try:
-                m=int(data)/1000
+                m=float(sensordata[0])
             except:
                 m=0
-            print(m)
-            #画面をクリア
-            plt.cla()
-            Y.append(m)
-            X.append(len(Y))
-            #xに100個以上格納されたら
-            if len(X) > 10:
-                xlim[0] += 1
-                xlim[1] += 1
 
-            Y2.append(0)
-            X2.append(len(Y2))
-            #xに100個以上格納されたら
-            if len(X2) > 10:
-                xlim2[0] += 1
-                xlim2[1] += 1
+            try:
+                temp=float(sensordata[1])
+            except:
+                temp=0
 
-            plt.plot(X, Y,color = 'blue')
-            plt.plot(X2, Y2,color = 'red')
-            #x軸、y軸方向の表示範囲を設定
-            plt.ylim(-1, 2)
-            plt.xlim(xlim[0], xlim[1])
-            #描写
-            plt.pause(0.01)
+            if isinstance(m, float) and isinstance(temp, float):
+                print(readdata)
+                with open(path, 'a') as f:
+                    dt_now = datetime.datetime.now()
+                    f.write(dt_now.strftime('%Y/%m/%d(%H:%M:%S),')+str(m)+","+str(1)+",\n")
+
+                #画面をクリア
+                plt.cla()
+                Y.append(m)
+                X.append(len(Y))
+                #xに100個以上格納されたら
+                if len(X) > 10:
+                    xlim[0] += 1
+                    xlim[1] += 1
+
+                Y2.append(temp)
+                X2.append(len(Y2))
+                #xに100個以上格納されたら
+                if len(X2) > 10:
+                    xlim2[0] += 1
+                    xlim2[1] += 1
+
+                plt.plot(X, Y,color = 'blue')
+                plt.plot(X2, Y2,color = 'red')
+                #x軸、y軸方向の表示範囲を設定
+                plt.ylim(-1, 2)
+                plt.xlim(xlim[0], xlim[1])
+                #描写
+                plt.pause(0.01)
 
         except KeyboardInterrupt:
             ser.close()
