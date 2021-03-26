5430dda82e56c0e3f55382233241895c9315e8a7
Raspy <eieioeiji0501@gmail.com>
2021-03-26 16:37:43 +0900

    change LINE sticker

--- project/LINE/main.py
+++ project/LINE/main.py
@@ -5,9 +5,9 @@ def main():
     bot.send(message="hellow world!")
     bot.send(
         message="Test!",
-        image='images.png',  # png or jpg
+        #image='images.png',  # png or jpg
         sticker_package_id=1,#スタンプ
-        sticker_id=17,#id 1 1~17
+        sticker_id=1,#id 1 1~17
         )
 
 
