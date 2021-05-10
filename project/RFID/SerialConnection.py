7e99c1e651a05d3b3ec330b418e5193ef40697a5
Nishizawa Eiji <eieioeiji0501@gmail.com>
2021-05-10 17:10:45 +0900

    add RFID project

---
+++ project/RFID/SerialConnection.py
@@ -0,0 +1,34 @@
+import serial
+from serial.tools import list_ports
+
+def select_port():
+    ser = serial.Serial()
+    ser.baudrate = 115200    # ArduinoのSerial.beginで指定した値
+    ser.timeout = 0.1       # タイムアウトの時間
+
+    ports = list_ports.comports()    # ポートデータを取得
+
+    devices = [info.device for info in ports]
+
+    if len(devices) == 0:
+        # シリアル通信できるデバイスが見つからなかった場合
+        print("error: device not found")
+        return None
+    elif len(devices) == 1:
+        print("only found %s" % devices[0])
+        ser.port = devices[0]
+    else:
+        # ポートが複数見つかった場合それらを表示し選択させる
+        for i in range(len(devices)):
+            print("input %3d: open %s" % (i,devices[i]))
+        print("input number of target port >> ",end="")
+        num = int(input())
+        ser.port = devices[num]
+
+    # 開いてみる
+    try:
+        ser.open()
+        return ser
+    except:
+        print("error when opening serial")
+        return None
---
+++ project/RFID/main.py
@@ -0,0 +1,58 @@
+
+import matplotlib.pyplot as plt
+from SerialConnection import select_port
+import numpy as np
+import datetime
+import re
+
+global end
+
+def hasNumbers(inputString):
+    return bool(re.search(r'\d', inputString))
+
+def main():#メイン関数
+    global end
+    end=False
+    print("Start!")
+    #Serial portの選択
+    ser = select_port()
+    if ser is None:
+        return#シリアルポートがないときプログラム終了
+    #ログファイルの作成(yyyymmddHHMMSS_Log.csv yyyy:年 mm:月 dd:日 HH:時 MM:分 SS:秒)
+    dt_now = datetime.datetime.now()#現在時刻の取得
+    path='data/'+dt_now.strftime('%Y%m%d%H%M%S_Log.csv')#ファイルのPath
+    with open(path, 'a') as f:
+        f.write("ID,\n")#ログの書き込み
+    ID=0
+    readID=0
+    while True:
+        try:
+            readdata=str(ser.readline().decode())
+            print(readdata)
+        except KeyboardInterrupt:#Press a key to begin scanning for tags.と表示されたらctrl + C
+            ser.write(b".\n")
+            break
+    print("Start!!\n\r")
+    datas = []
+    while True:
+        if end==True:     # グラフ上で右クリックしたとき終了
+            break
+        try:
+            ser.reset_input_buffer()#受信バッファをクリア
+            readdata=str(ser.readline().decode()).strip()#受信データをデコードして文字列に変換
+            if hasNumbers(readdata) and any(map(str.isdigit, readdata)):
+                readID=readdata
+            if readID!=ID:
+                ID=readID
+                print(ID)
+                #データの保存
+                with open(path, 'a') as f:
+                    f.write(str(ID)+",\n")#ログの書き込み
+        except KeyboardInterrupt:
+            break
+    #終了処理
+    print("End")
+    ser.close()#シリアルポート停止
+
+if __name__ == "__main__":
+    main()
